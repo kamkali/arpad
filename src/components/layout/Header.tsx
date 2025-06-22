217ff9c2b889ed9e8bf6f19b3036e59c18cc6193
Kamil Kaliś <kamkalis@gmail.com>
2025-06-22 19:55:26 +0000

    Proszę przygotuj lepsze logo ARPAD. Teraz główną literą jest V, potrzebu

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,11 +9,10 @@ import { Menu, ArrowRight } from 'lucide-react';
 import React from 'react';
 
 const ArpadLogo = () => (
-    <svg width="120" height="40" viewBox="0 0 130 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M23.0132 35.5L14.7132 10.3H19.7132L25.3132 28.1L30.9132 10.3H35.8132L27.5132 35.5H23.0132Z" fill="#023047"/>
-        <path d="M0 35.5V5.5H5V35.5H0Z" fill="#219EBC"/>
-        <path d="M0 5.5C0 2.46243 2.46243 0 5.5 0H33.5C36.5376 0 39 2.46243 39 5.5V8.5H34V5.5C34 5.23478 33.8946 4.98043 33.7071 4.79289C33.5196 4.60536 33.2652 4.5 33 4.5H6C5.73478 4.5 5.48043 4.60536 5.29289 4.79289C5.10536 4.98043 5 5.23478 5 5.5V35.5H0V5.5Z" fill="#219EBC"/>
-        <text x="45" y="28" fontFamily="Poppins, sans-serif" fontSize="24" fontWeight="bold" fill="#023047">ARPAD</text>
+    <svg width="130" height="40" viewBox="0 0 130 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M25.6667 0L0 40H8.55556L30.3333 6.66667L25.6667 0Z" fill="#219EBC"/>
+        <path d="M17.1111 0L42.7778 40H34.2222L12.4444 6.66667L17.1111 0Z" fill="#023047" fillOpacity="0.9"/>
+        <text x="50" y="28" fontFamily="Poppins, sans-serif" fontSize="24" fontWeight="bold" fill="#023047">ARPAD</text>
     </svg>
 );
 
